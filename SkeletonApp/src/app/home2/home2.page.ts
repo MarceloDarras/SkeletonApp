e29df4ae98b1cace31b7f6bcb1588cbab5c5e65f
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-08-22 12:05:33 -0400

    Add files via upload

--- SkeletonApp/src/app/home2/home2.page.ts
+++ SkeletonApp/src/app/home2/home2.page.ts
@@ -7,11 +7,39 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./home2.page.scss'],
 })
 export class Home2Page implements OnInit {
+  campoSeleccionado: string="";
+  texto1:string="";
+  texto2: string="";
+
+  campoSeleccOriginal:string = "";
+  texto1Original: string="";
+  texto2Original: string="";
+
+  
+
+  nombreCampo: string = 'Nivel Educacional';
   nombre: string="";
   constructor(private route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
       this.nombre = params['username'];
-    })
+    }) 
+    this.texto2Original = this.texto2;
+    this.campoSeleccOriginal = this.campoSeleccionado;
+    this.texto1Original = this.texto1;
+
+  }
+
+  limpiar(){
+    this.campoSeleccionado = '';
+    this.texto1= '';
+    this.texto2= '';
+  }
+
+  mostrar(){
+    alert(this.texto2Original)
+    this.campoSeleccionado = this.campoSeleccOriginal;
+    this.texto1 = this.texto1Original;
+    this.texto2 = this.texto2Original;
   }
 
   ngOnInit() {
