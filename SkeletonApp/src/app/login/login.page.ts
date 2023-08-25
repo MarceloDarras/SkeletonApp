4c3d45d0053dcf4e08635dd85b20b4dd7e84df46
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-08-25 10:30:56 -0400

    Add files via upload

--- SkeletonApp/src/app/login/login.page.ts
+++ SkeletonApp/src/app/login/login.page.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
-import { NavController, AnimationController, IonCard, Animation } from '@ionic/angular';
+import { Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import type { QueryList } from '@angular/core';
+import type { Animation } from '@ionic/angular';
+import { NavController, AnimationController, IonCard } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -11,7 +13,9 @@ export class LoginPage implements OnInit {
   nombre: string="";
   contrasena: string="";
   
-  private cardA!: Animation;
+  private animation!: Animation;
+  private animation2!: Animation;
+  private animationExito!:Animation;
   private cardB!: Animation;
   private cardC!: Animation;
 
@@ -21,42 +25,82 @@ export class LoginPage implements OnInit {
   }
 
   ngAfterViewInit() {
-    this.cardA = this.animationCtrl
+    const cardA = this.animationCtrl
       .create()
-      .addElement(this.cardElements.get(0).nativeElement)
-      .fill('none')
-      .duration(1000)
-      .keyframes([
-        { offset: 0, transform: 'scale(1) rotate(0)' },
-        { offset: 0.5, transform: 'scale(1.2) rotate(45deg)' },
-        { offset: 1, transform: 'scale(1) rotate(0)' },
-      ]);
+      .addElement(this.cardElements.first.nativeElement)
+      .duration(1500)
+      .iterations(1)
+      .direction('alternate')
+      .fromTo('background', 'white', 'Green');
+    const cardB = this.animationCtrl
+        .create()
+        .addElement(this.cardElements.last.nativeElement)
+        .duration(500)
+        .beforeStyles({
+          filter: 'invert(75%)',
+        })
+        .beforeClearStyles(['box-shadow'])
+        .afterStyles({
+          'box-shadow': 'rgba(255, 0, 50, 0.4) 0px 4px 16px 6px',
+        })
+        .afterClearStyles(['filter'])
+        .keyframes([
+          { offset: 0, transform: 'scale(1)' },
+          { offset: 0.5, transform: 'scale(1.5)' },
+          { offset: 1, transform: 'scale(1)' },
+        ]);
 
-    this.cardB = this.animationCtrl
-      .create()
-      .addElement(this.cardElements.get(1).nativeElement)
-      .fill('none')
-      .duration(1000)
-      .keyframes([
-        { offset: 0, transform: 'scale(1)', opacity: '1' },
-        { offset: 0.5, transform: 'scale(1.2)', opacity: '0.3' },
-        { offset: 1, transform: 'scale(1)', opacity: '1' },
-      ]);
+    const cardC = this.animationCtrl
+        .create()
+        .addElement(this.cardElements.last.nativeElement)
+        .duration(500)
+        .beforeStyles({
+          filter: 'invert(75%)',
+        })
+        .beforeClearStyles(['box-shadow'])
+        .afterStyles({
+          'box-shadow': 'rgba(50, 255, 0, 0.4) 0px 4px 16px 6px',
+        })
+        .afterClearStyles(['filter'])
+        .keyframes([
+          { offset: 0, transform: 'scale(1)' },
+          { offset: 0.5, transform: 'scale(1.5)' },
+          { offset: 1, transform: 'scale(1)' },
+        ]);
 
-    this.cardC = this.animationCtrl
+      this.animation = this.animationCtrl
       .create()
-      .addElement(this.cardElements.get(2).nativeElement)
-      .fill('none')
-      .duration(1000)
-      .keyframes([
-        { offset: 0, transform: 'scale(1)', opacity: '0.5' },
-        { offset: 0.5, transform: 'scale(0.8)', opacity: '1' },
-        { offset: 1, transform: 'scale(1)', opacity: '0.5' },
-      ]);
+      .duration(500)
+      .iterations(1)
+      .addAnimation([cardA]);
+
+      this.animation2 = this.animationCtrl
+      .create()
+      .duration(500)
+      .iterations(2)
+      .addAnimation([cardB]);
+
+      this.animationExito = this.animationCtrl
+      .create()
+      .duration(500)
+      .iterations(2)
+      .addAnimation([cardB]);
+
+  }
+
+  play() {
+      this.animation.play();
+  }
+
+  play2() {
+    
+    this.animation2.play();
   }
 
   login(){
     if(this.nombre != "" && this.contrasena != ""){
+      this.animation.play();
+      this.animationExito.play();
       this.navCtrl.navigateForward('/home2', {
         queryParams:{
           username: this.nombre
@@ -65,14 +109,9 @@ export class LoginPage implements OnInit {
       alert("Bienvenido " + this.nombre);
     }else if(this.contrasena.length<4 || this.contrasena.length>15){
       document.getElementById("contrasena");
-      alert("La contraseña no cumple con los parametros");
+      this.animation2.play();
     }else{
-      let contra = document.getElementById("contraseña")?.innerText;
-      console.log(contra);
       
-      if (contra){
-
-      }
     }
   }
 
